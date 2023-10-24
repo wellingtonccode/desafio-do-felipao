578915b5716d56f831a2c09a4fd29e12149cbba6
wellingtonccode <wellingtoncmp@hotmail.com>
2023-10-24 14:36:43 -0300

    Proposta de solução para desafio.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Oi");
+let nome = 'Hércules'
+    , xp = 10002
+    , achei = false
+    ;
+
+const niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal"]
+    , vlrs = [1000, 2000, 5000, 7000, 8000, 9000, 10000]
+    ;
+
+for (let index = 0; index < vlrs.length; index++) {
+    if (xp <= vlrs[index]){
+        achei = true;
+        console.log(`O Herói de nome ${nome} está no nível de ${niveis[index]}`);
+        break;
+    }
+}
+
+if(!achei){
+    console.log(`O Herói de nome ${nome} está no nível de Radiante`);
+}
